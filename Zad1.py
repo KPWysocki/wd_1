82372c7ea0b26101aca8b851351387c9f306a119
kwysocki <krzysiek27-052@o2.pl>
2020-02-29 12:05:30 +0100

    koniec zajec

---
+++ Zad1.py
@@ -0,0 +1,36 @@
+print("Hello world!")
+
+# def main():
+#     pass
+
+
+# łańcuchy znaków
+
+liczba = "Ala"
+liczba = 0
+liczba = 0.0
+
+# PEP8
+
+imie = "Ala"
+print(imie)
+print(type(imie))
+print(type(5))
+print(type(5.6))
+print(type(True))
+print(type(None)) # null
+
+imie = str("Ala")
+liczba = str(100)
+
+# <class 'str' >
+# <class 'int' >
+# <class 'float' >
+# <class 'bool' >
+# <class 'NoneType' >
+
+binarnie = "101"
+print(int(binarnie,2))
+
+imie.lower()
+print(imie)
